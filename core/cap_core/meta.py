f68ccdf9575db01bd415a1e2cab5d584f0975b0c
Domas Leščinskas <domas.lescinskas@gmail.com>
2026-02-08 20:57:59 +0200

    Updated project version

--- core/cap_core/meta.py
+++ core/cap_core/meta.py
@@ -1,3 +1,3 @@
 """Metadata for CAP core package."""
 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
--- shells/cli/cap_cli/meta.py
+++ shells/cli/cap_cli/meta.py
@@ -1,3 +1,3 @@
 """Metadata for CAP CLI package."""
 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
